f36166f021452719950c8040912ab2e27e7ab750
GGolfz <wisarutgolf11@gmail.com>
2022-05-19 20:46:51 +0700

    update: export path :sparkles:

--- server/service/data.py
+++ server/service/data.py
@@ -10,9 +10,9 @@ class Database:
         return self.data
     def save_data(self, data):
         all_data = []
-        with open("data.json", "r") as f:
+        with open("combine-result/data.json", "r") as f:
             all_data = json.load(f)
         all_data.append(data)
-        with open("data.json", "w") as f:
+        with open("combine-result/data.json", "w") as f:
             json.dump(all_data, f)
         
