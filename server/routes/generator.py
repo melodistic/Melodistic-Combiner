92dcbec2b8da4810e20a695fee7e318ef37c25bb
GGolfz <wisarutgolf11@gmail.com>
2022-07-09 22:04:18 +0700

    update: controller :sparkles:

--- server/routes/generator.py
+++ server/routes/generator.py
@@ -5,6 +5,7 @@ class Generator(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('program_name',type=str)
         parser.add_argument('program_image_url',type=str)
+        parser.add_argument('exercise_type',type=str)
         parser.add_argument('sections',type=list,location="json")
         program=parser.parse_args()
         return generate(program)
